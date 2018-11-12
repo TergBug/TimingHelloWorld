202b2aa20a6d4d12e76aaa0f0cd5436b7ef1a70d
TergBug <tergbug@gmail.com>
2018-11-12 22:50:59 +0200

    0.0.1-SNAPSHOT

--- src/main/java/tduty/helloworld/App.java
+++ src/main/java/tduty/helloworld/App.java
@@ -9,23 +9,24 @@ import org.apache.logging.log4j.LogManager;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 	private static final Logger log = LogManager.getLogger(App.class.getName());
-	public static String hello_world(LocalTime in) {
-    	if(in.isAfter(LocalTime.of(5, 59)) && in.isBefore(LocalTime.of(9, 0))) {
+	
+	public static String getTimeOfDay(LocalTime in) {
+    	if(in.isAfter(LocalTime.of(5, 59, 59, 999999999)) && in.isBefore(LocalTime.of(9, 0, 0, 0))) {
     		return "morning";
     	}
-    	else if(in.isAfter(LocalTime.of(8, 59)) && in.isBefore(LocalTime.of(19, 0))) {
+    	else if(in.isAfter(LocalTime.of(8, 59, 59, 999999999)) && in.isBefore(LocalTime.of(19, 0, 0, 0))) {
     		return "day";
     	}
-    	else if(in.isAfter(LocalTime.of(18, 59)) && in.isBefore(LocalTime.of(23, 0))) {
+    	else if(in.isAfter(LocalTime.of(18, 59, 59, 999999999)) && in.isBefore(LocalTime.of(23, 0, 0, 0))) {
     		return "evening";
     	}
     	else {
     		return "night";
     	}
 	}
+	
     public static void main( String[] args ){
     	log.debug("---Starting program---");
     	log.debug("Create object ResourceBundle");
@@ -33,7 +34,7 @@ public class App
     	log.debug("Get current time");
     	LocalTime lt = LocalTime.now();
     	log.debug("Get key-word for time "+lt.toString());
-    	String key = hello_world(lt);
+    	String key = getTimeOfDay(lt);
     	log.debug("Get string from message resource on \""+key+"\" key");
     	String s = rb.getString(key);
     	log.debug("Display this string in console");
--- src/test/java/tduty/helloworld/Tests.java
+++ src/test/java/tduty/helloworld/Tests.java
@@ -10,29 +10,25 @@ import org.junit.Test;
 
 public class Tests {
 	private static final Logger log = LogManager.getLogger(Tests.class.getName());
+	
 	@Test
-	public final void testHello_world() {
+	public final void testGetTimeOfDay() {
 		for(int h = 0; h<24; h++) {
 			for(int m = 0; m<60; m++) {
+				log.info("Test for time "+h+":"+m);
 				if((h>=6&&h<9) && (m>=0&&m<60)) {
-					log.info("Test for time "+h+":"+m);
-					assertEquals(h+":"+m+" is morning", "morning", App.hello_world(LocalTime.of(h, m)));
+					assertEquals(h+":"+m+" is morning", "morning", App.getTimeOfDay(LocalTime.of(h, m)));
 				}
 				else if((h>=9&&h<19) && (m>=0&&m<60)) {
-					log.info("Test for time "+h+":"+m);
-					assertEquals(h+":"+m+" is day", "day", App.hello_world(LocalTime.of(h, m)));
+					assertEquals(h+":"+m+" is day", "day", App.getTimeOfDay(LocalTime.of(h, m)));
 				}
 				else if((h>=19&&h<23) && (m>=0&&m<60)) {
-					log.info("Test for time "+h+":"+m);
-					assertEquals(h+":"+m+" is evening", "evening", App.hello_world(LocalTime.of(h, m)));
+					assertEquals(h+":"+m+" is evening", "evening", App.getTimeOfDay(LocalTime.of(h, m)));
 				}
-				else if(((h>=23&&h<24)||(h<6&&h>=0)) && (m>=0&&m<60)) {
-					log.info("Test for time "+h+":"+m);
-					assertEquals(h+":"+m+" is night", "night", App.hello_world(LocalTime.of(h, m)));
+				else if(((h>=23 && h<24)||(h>=0&&h<6)) && (m>=0&&m<60)) {
+					assertEquals(h+":"+m+" is night", "night", App.getTimeOfDay(LocalTime.of(h, m)));
 				}
 			}
 		}
-		
-		
 	}
 }
