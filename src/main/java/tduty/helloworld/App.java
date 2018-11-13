a6a8b3355ddbf38fbaa9791ac1ae3da5d4bf3b7b
Terg <35434168+TergBug@users.noreply.github.com>
2018-11-13 21:27:53 +0200

    0.0.1-SNAPSHOT

--- src/main/java/tduty/helloworld/App.java
+++ src/main/java/tduty/helloworld/App.java
@@ -27,7 +27,7 @@ public class App {
 		}
 	}
 	
-	public static void main( String[] args ){
+	public static void main(String[] args) {
 		log.debug("---Starting program---");
 		log.debug("Create object ResourceBundle");
 		ResourceBundle rb = ResourceBundle.getBundle("message-resource");
